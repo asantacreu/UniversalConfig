dd0e71d8b19ab6f0ed1107a27243f8f0c062a71d
Aniol Santacreu <aniolsantacreu_1@hotmail.com>
2019-08-18 13:26:32 +0200

    Prepares UnitTest environment

---
+++ test/UConfigUnitTest.cpp
@@ -0,0 +1,18 @@
+
+
+#include "UConfig.h"
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+
+class UConfigUnitTest : public testing::Test {
+	protected:
+		virtual void SetUp() {}
+		virtual void TearDown() {}
+};
+
+
+
+TEST_F(UConfigUnitTest, xxx) {
+}
